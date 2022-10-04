76d868cc6d4796662a5bfa07dff4d99b615b06e0
Vu Truong <vu.truong.work@gmail.com>
2022-10-04 21:13:56 +0700

    - update

--- PayrollCaseStudy.PayrollDomain/Affiliation.cs
+++ PayrollCaseStudy.PayrollDomain/Affiliation.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
+﻿
 namespace PayrollCaseStudy.PayrollDomain {
     public interface Affiliation {
         decimal CalculateDeductions(Paycheck paycheck);
---
+++ PayrollCaseStudy.TransactionApplication/TransactionApplication.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace PayrollCaseStudy.TransactionApplication {
+    public class TransactionApplicaion {
+        readonly TransactionSource _source;
+        public TransactionApplicaion(TransactionSource transactionSource) {
+            _source= transactionSource;
+            
+        }
+
+        [DebuggerStepThrough]
+        public void Process() {
+            while(true) { 
+                Transaction transaction;
+                try {
+                    transaction = _source.Next();
+                }
+                catch (Exception e) {
+                    Console.Error.WriteLine("Failed processing line:\n{0}", e);
+                    continue;
+                }
+
+                if(transaction == null) {
+                    return;
+                }
+                transaction.Execute();
+            }
+        }
+    }
+}
