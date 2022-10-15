97f27360d7a1757f21e3f2b325d909837b8af2b1
Vu Truong <vu.truong.work@gmail.com>
2022-10-15 21:30:20 +0700

    - update

--- PayrollCaseStudy.PayrollApplication/PayrollApplication.cs
+++ PayrollCaseStudy.PayrollApplication/PayrollApplication.cs
@@ -3,6 +3,22 @@ using System;
 using System.Diagnostics;
 
 namespace PayrollCaseStudy.PayrollApplication {
+/*
+The PayrollApplication component contains the
+PayrollApplication and TransactionSource classes. Both of these two classes
+depend on the abstract Transaction class, which is in the PayrollDomain component.
+Note that the TextParserTransactionSource class is in another component that
+depends on the abstract PayrollApplication class. This creates an upside-down structure 
+in which the details depend on the generalities, and the generalities are independent.
+This conforms to DIP.
+
+The most striking case of generality and independence is the PayrollDomain component. 
+This component contains the essence of the whole system, yet it depends upon
+nothing! Examine this component carefully. It contains Employee, PaymentClassification, 
+PaymentMethod, PaymentSchedule, Affiliation, and Transaction.
+This component contains all the major abstractions in our model, yet it has no dependencies. 
+Why? Because all the classes it contains are abstract
+*/
     public class PayrollApplication {
         readonly TransactionSource _source;
         public PayrollApplication(TransactionSource transactionSource) {
